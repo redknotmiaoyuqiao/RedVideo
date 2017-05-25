39d2fc42271e95725163858e0fdbaea21ad50774
yinhuiyao <yaoyao@qq.com>
2017-05-25 10:15:36 +0800

    gl

--- QtRedChatRoom/main.cpp
+++ QtRedChatRoom/main.cpp
@@ -14,16 +14,20 @@ int main(int argc, char *argv[])
     H264encode * h264encode = new H264encode();
     h264encode->h264_encoder_init(width,height);
 
+    unsigned char * out = (uint8_t *) malloc(sizeof(uint8_t) * width * height * 3);
+
     int i=0;
-    while(i<10){
+    while(i<1000){
         unsigned char * data = camera->read_frame();
-        unsigned char * out = (uint8_t *) malloc(sizeof(uint8_t) * width * height * 3);
+
         int res = h264encode->h264_compress_frame(10,data,out);
 
-        printf("Res::::%d\n\n",res);
 
         i++;
     }
+
+    //printf("Res::::%d\n\n",res);
+
 
 
 //    for(int i=0;i<width * height * 3 / 2;i++){
